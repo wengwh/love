a0885771b1fc8e4cffddae7ab853493a13fa3eb2
wengwh <415554484@qq.com>
2018-07-01 01:00:31 +0800

    优化大小

--- scripts/app.js
+++ scripts/app.js
@@ -51,3 +51,76 @@
         numeratio: !0
     })
 } (jQuery);
+
+
+var together = new Date();
+var strs = "2017-11-14 00:00:00";
+var nowTime = new Date();
+var together = new Date(strs.replace(/-/g, "/"));
+timeElapse(together);
+setInterval(function() {
+	timeElapse(together);
+}, 500);
+function timeElapse(c) {
+	var e = Date();
+	var f = (Date.parse(e) - Date.parse(c)) / 1000;
+	var g = Math.floor(f / (3600 * 24));
+	f = f % (3600 * 24);
+	var b = Math.floor(f / 3600);
+	if (b < 10) {
+		b = "0" + b
+	}
+	f = f % 3600;
+	var d = Math.floor(f / 60);
+	if (d < 10) {
+		d = "0" + d
+	}
+	f = f % 60;
+	if (f < 10) {
+		f = "0" + f
+	}
+	var a = '<span class="digit">' + g + '</span> 天 <span class="digit">' + b
+			+ '</span> 时 <span class="digit">' + d
+			+ '</span> 分 <span class="digit">' + f + "</span> 秒";
+	$("#elapseClock").html(a)
+};
+
+//获取picid函数
+$(function() {
+	$("area").click(function() {
+		var picId = $(this).attr("data-id");
+		var picSrc = "images/img_big/" + picId + ".jpg"
+		$(".modal-content>.pic-box>img").attr("src", picSrc);
+	})
+	var isPlaying = function(audio) {
+		return !audio.paused;
+	}
+	var a = document.getElementById('main_audio');
+	$('#btn-play').on('click', function() {
+		if ($(this).hasClass('rotate')) {
+			a.pause();
+			$(this).removeClass('rotate');
+		} else {
+			a.play();
+			$(this).addClass('rotate');
+		}
+	});
+
+})
+
+
+function autoPlayAudio() {
+	wx.config({
+		// 配置信息, 即使不正确也能使用 wx.ready
+		debug : false,
+		appId : '',
+		timestamp : 1,
+		nonceStr : '',
+		signature : '',
+		jsApiList : []
+	});
+	wx.ready(function() {
+		document.getElementById('main_audio').play();
+	});
+}
+autoPlayAudio(); // 推荐使用方法1
